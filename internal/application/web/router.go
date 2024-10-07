ef257a9e8249e016962549e28781cae954d5d154
bary822 <hiroto@hey.com>
2024-10-07 16:49:12 +0900

    Add production domain to the whitelist

--- internal/application/web/router.go
+++ internal/application/web/router.go
@@ -220,7 +220,7 @@ func (h *EditMemoHandler) Handle(w http.ResponseWriter, r *http.Request) {
 
 func HandleCORSPreflight(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173, http://app.gomemon.bary822.me")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 
